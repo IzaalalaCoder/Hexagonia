7a2f4cafa7f7432a21925c7fe0de762757185bb9
Izee <izana.khabouri@univ-rouen.fr>
2024-04-21 15:21:04 +0200

    implents negamax

--- hex/src/main/java/hex/model/game/theoric/algorithm/max/NegaMax.java
+++ hex/src/main/java/hex/model/game/theoric/algorithm/max/NegaMax.java
@@ -1,23 +1,56 @@
 package hex.model.game.theoric.algorithm.max;
 
+import hex.model.board.Board;
 import hex.model.board.cell.Cell;
+import hex.model.game.player.computer.Level;
 import hex.model.game.theoric.algorithm.Theory;
+import hex.model.game.theoric.algorithm.tree.Arborescence;
 import hex.model.game.theoric.structure.Node;
+import hex.model.game.theoric.structure.util.LabelPlayer;
+import hex.model.game.theoric.structure.util.Status;
 
-public class NegaMax implements Theory {
+public class Negamax implements Theory {
 
-    
+    // ATTRIBUTES
+
+    private Cell chooseCell;
+
+    // CONSTRUCTOR
+
+    public Negamax(Board board, Level level) {
+        Arborescence a = new Arborescence(board);
+        this.analyze(a.createArborescence(level));
+        chooseCell = null;
+    }   
+
+    // REQUESTS
 
     @Override
     public Cell getChooseCell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getChooseCell'");
+        return chooseCell;
+        
     }
+
+    // COMMANDS
 
     @Override
     public void analyze(Node root) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyze'");
+        this.negamax(root);
     }
+
+    // UTILS
     
+    private Double negamax(Node root) {
+        Double val;
+        if (root.getStatus() == Status.LEAF) {
+            // calculate heuristic value
+            val = root.getHeuristicValue();
+        } else {
+            val = Double.MIN_VALUE;
+            for (Node n : root.getSuccessor()) {
+                val = Math.max(val, -this.negamax(n));
+            }
+        }
+        return val;
+    }
 }
