0ce12cc364bc77d91152e33d2c1f570c9a184493
Izee <izana.khabouri@univ-rouen.fr>
2024-04-21 15:14:01 +0200

    add file for negamax

---
+++ hex/src/main/java/hex/model/game/theoric/algorithm/max/NegaMax.java
@@ -0,0 +1,23 @@
+package hex.model.game.theoric.algorithm.max;
+
+import hex.model.board.cell.Cell;
+import hex.model.game.theoric.algorithm.Theory;
+import hex.model.game.theoric.structure.Node;
+
+public class NegaMax implements Theory {
+
+    
+
+    @Override
+    public Cell getChooseCell() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getChooseCell'");
+    }
+
+    @Override
+    public void analyze(Node root) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'analyze'");
+    }
+    
+}
