fe2af56230694dc40a1ef019437ed2fbb392e212
Izaalala <izana.khabouri@univ-rouen.fr>
2024-04-26 12:06:03 +0200

    remove unused import

--- hex/src/main/java/hex/model/game/player/computer/Play.java
+++ hex/src/main/java/hex/model/game/player/computer/Play.java
@@ -1,13 +1,11 @@
 package hex.model.game.player.computer;
 
-import hex.model.board.Board;
 import hex.model.board.cell.Cell;
 import hex.model.board.cell.State;
 import hex.model.game.Action;
 import hex.model.game.Game;
 import hex.model.game.theoric.algorithm.RandomAction;
 import hex.model.game.theoric.algorithm.Theory;
-import hex.model.game.theoric.algorithm.max.Minimax;
 import hex.model.game.theoric.algorithm.sss.ThreeSStar;
 
 public class Play {
